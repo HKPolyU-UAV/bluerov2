761196ca316462e9336798d197e0d1b49858a8da
YuanboXue <15651860528@163.com>
2024-04-03 22:17:18 +0800

    asdfsadfsa

--- bluerov2_states/include/ImuDo.h
+++ bluerov2_states/include/ImuDo.h
@@ -9,7 +9,7 @@
     bluerov2 is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
+    GNU General Public License for more details.sdasdasdas
 
     You should have received a copy of the GNU General Public License
     along with bluerov2.  If not, see <http://www.gnu.org/licenses/>.
